901713de034e5533d38dce1bafc0fbd972a2d2de
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2023-09-25 22:49:35 +0300

    Update Lemmatization.py

--- Natural Language Processing Coding Works and Projects/Lemmatization.py
+++ Natural Language Processing Coding Works and Projects/Lemmatization.py
@@ -3,6 +3,9 @@
 
 # In[1]:
 
+# Lemmatization is a text pre-processing technique used in natural language processing (NLP) models 
+# to break a word down to its root meaning to identify similarities. For example, a lemmatization 
+# algorithm would reduce the word better to its root word, or lemme, good.  
 
 # In contrast to stemming, lemmatization looks beyond word reduction and considers a language's full vocabulary 
 # to apply a morphological analysis to words.
